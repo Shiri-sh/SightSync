01320d44090a0929d60989ebc4aab368fcaf0821
shiri shahor <shirishahor@gmail.com>
2026-01-11 23:01:52 +0200

    add endpoint

--- server/server.py
+++ server/server.py
@@ -2,30 +2,57 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from services import *
+from ClipScorer import ClipScorer
 app = FastAPI(title="SIGHTSYNC MVP")
 
-# CORS – חובה בשביל קליינט
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ל-MVP בלבד
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.post("/verify")
-async def verify(
-    image: UploadFile = File(...),
-    text: str = Form(...)
+clipScorer = ClipScorer()
+
+@app.post("/upload_img")
+async def upload_img(
+    image:UploadFile=File(...)
 ):
     img = Image.open(image.file)
-    score = clip_score(img, text)
+    
+    return {"filename": image.filename}
+
+@app.post("/clip_score")
+async def clip_score(
+    image_name: str=Form(...),
+    text: str = Form(...)
+):
+    img = Image.open(image_name)
+    
+    score = clipScorer.score(img, text)
+    status_score=get_status_from_score(score)
     return {
         "status": "ok",
-        "filename": image.filename,
+        "filename": image_name,
         "text_received": text,
-        "verdict": "match",
-        "confidence": 0.99
+        "status": status_score,
+        "score": score
+    }
+@app.get("/blip_img_anlz")
+async def blip_img_anlz(
+    image_name: str=Form(...),
+    text: str = Form(...),
+    score: float = Form(...)
+):
+    img = Image.open(image_name)
+    
+    return {
+        "status": "ok",
+        "filename": image_name,
+        "text_received": text,
+        "text_blip": "description from blip",
+        "clip_score": score
     }
 @app.get("/")
 async def read_root():
