c7475924a9bfd667c00d2e1c58d090bc612e9fd3
shiri shahor <shirishahor@gmail.com>
2026-01-13 12:41:59 +0200

    spaces

--- server/routes/clip.py
+++ server/routes/clip.py
@@ -2,5 +2,4 @@ from fastapi import APIRouter
 from controllers.clip import clip_score
 
 router = APIRouter()
-
 router.post("/clip_score")(clip_score)
--- server/routes/data.py
+++ server/routes/data.py
@@ -2,5 +2,4 @@ from fastapi import APIRouter
 from controllers.data import upload_img
 
 router = APIRouter()
-
 router.post("/upload_img")(upload_img)
