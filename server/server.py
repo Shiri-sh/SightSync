ac8e6a06695abfe60ee754dfd2fb1041ecd5f0ce
shiri shahor <shirishahor@gmail.com>
2026-01-13 12:53:54 +0200

    update

--- server/server.py
+++ server/server.py
@@ -1,10 +1,21 @@
 
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from services import *
-from server.interface.ClipScorer import ClipScorer
 
-app = FastAPI(title="SIGHTSYNC MVP")
+from routes.blip import router as blip_router
+from routes.clip import router as clip_router
+from routes.data import router as data_router
+
+from interface.ClipScorer import ClipScorer
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.clip_scorer = ClipScorer()
+    yield
+    app.state.clip_scorer = None
+
+app = FastAPI(title="SIGHTSYNC MVP", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,12 +25,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# clipScorer = ClipScorer()
-
-
-
-
-
 @app.get("/")
-async def read_root():
+async def read_root():    
     return "server running"
+
+app.include_router(blip_router, prefix="/blip")
+app.include_router(clip_router, prefix="/clip")
+app.include_router(data_router, prefix="/data")
+
