f85b8f1b332cc2f3d6990b5162879c0963dc72b6
shiri shahor <shirishahor@gmail.com>
2026-01-15 14:05:58 +0200

    print

--- server/controllers/clip.py
+++ server/controllers/clip.py
@@ -9,19 +9,24 @@ from services import get_status_from_score
 IMAGE_DIR = "images/"
 async def clip_score(
     request: Request,
-    image_name: str=Form(...),
-    text: str = Form(...)
+    # image_name: str,
+    # text: str
+    data: dict
 ):
     try:
-        img = Image.open(f"{IMAGE_DIR}/{image_name}")
+        print("data:", data)
+        print("image_name:", data['image_name'])
+        print("text:", data['text'])
+        
+        img = Image.open(f"{IMAGE_DIR}/{data['image_name']}")
 
-        score = request.app.state.clip_scorer.score(img, text)
-        status_score=get_status_from_score(score)
+        score = request.app.state.clip_scorer.score(img, data['text'])
+        status_score = get_status_from_score(score)
 
         return {
             "status": "ok",
-            "filename": image_name,
-            "text_received": text,
+            "filename": data['image_name'],
+            "text_received": data['text'],
             "status": status_score,
             "score": score
         }
