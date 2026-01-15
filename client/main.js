044bcfe110331bd661a223ff29a8297b6751425c
shiri shahor <shirishahor@gmail.com>
2026-01-15 14:00:50 +0200

    saperate functions

--- client/main.js
+++ client/main.js
@@ -5,6 +5,29 @@ const result = document.getElementById("result");
 const preview = document.getElementById('imagePreview');
 
 let filename;
+async function uploadImage() {
+  if (!imageInput.files || imageInput.files.length === 0) return;
+
+  try{
+      const uploadForm = new FormData();
+      uploadForm.append('image', imageInput.files[0]);
+
+      const dataResponse= await fetch("http://localhost:8000/data/upload_img", {
+      method: "POST",
+      body: uploadForm,
+    });
+      const data=await dataResponse.json();
+      if(data.status==="ok"){
+        filename=data.filename;
+      }
+      else{
+          throw new Error(data);
+      }
+
+  }catch (error){
+    console.log("Error uploading image:", error.message);
+  }
+}
 imageInput.addEventListener('change', function(e) {
       const input = e.target;
       const file = input?.files?.[0];
@@ -24,36 +47,19 @@ imageInput.addEventListener('change', function(e) {
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  if (!imageInput.files || imageInput.files.length === 0) return;
-
-  try{
-      const uploadForm = new FormData();
-      uploadForm.append('image', imageInput.files[0]);
-
-      const dataResponse= await fetch("http://localhost:8000/data/upload_img", {
-      method: "POST",
-      body: uploadForm,
-    });
-      const data=await dataResponse.json();
-      if(data.status==="ok"){
-        filename=data.filename;
-      }
-      else{
-          throw new Error(data.message);
-      }
-
-  }catch (error){
-    console.log("Error uploading image:", error);
-  }
-
+  //await uploadImage();
+  filename="DSC_6831-258.jpg"
   if (filename) {
     try {
       const clipScoreResponse = await fetch("http://localhost:8000/clip/clip_score", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({image: filename, text: textInput.value}),
+        body: JSON.stringify({image_name: filename, text: textInput.value}),
       });
       const data = await clipScoreResponse.json();
+
+      console.log("Received data:", data);
+
       if(data.status==="ok"){
           result.textContent = JSON.stringify(data, null, 2);
       }
@@ -61,7 +67,7 @@ form.addEventListener("submit", async (e) => {
           throw new Error(data.message);
       }
     }catch (error){
-        console.log("Error getting clip score:", error);
+        console.log("Error getting clip score:", error.message);
     }
   }
 
