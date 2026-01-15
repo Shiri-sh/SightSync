68dbbd63fa620fa88bf5f9704c56f2935e365e6c
shiri shahor <shirishahor@gmail.com>
2026-01-15 14:01:31 +0200

    update

--- server/interface/ClipScorer.py
+++ server/interface/ClipScorer.py
@@ -15,5 +15,5 @@ class ClipScorer:
     def score(self, img, text):
         inputs = self.processor(text=[text], images=img, return_tensors="pt", padding=True)
         outputs = self.model(**inputs)
-        logits_per_image = outputs.logits_per_image
-        return logits_per_image
+        logits_per_image_text = outputs.logits_per_image
+        return logits_per_image_text
