b8f06466941efe5057110c10c8713d41a65856ae
shiri shahor <shirishahor@gmail.com>
2026-01-16 14:17:06 +0200

    GET data

--- server/controllers/data.py
+++ server/controllers/data.py
@@ -14,3 +14,19 @@ async def upload_img(
             return {"status": "ok", "filename": image.filename}
     except Exception as e:
         return {"status": "error", "message": str(e)}
+async def get_img(
+    filename: str = Form(...)
+):
+    try:
+        path = f"{DIR_PATH}/{filename}"
+        with open(path, "rb") as f:
+            return {"status": "ok", "filename": filename, "image": f.read()}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+    
+async def list_images():
+    try:
+        images = os.listdir(DIR_PATH)
+        return {"status": "ok", "images": images}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
--- server/routes/data.py
+++ server/routes/data.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from controllers.data import upload_img
+from controllers.data import upload_img, list_images, get_img
 
 router = APIRouter()
 router.post("/upload_img")(upload_img)
+router.get("/list_images")(list_images)
+router.post("/get_img")(get_img)
