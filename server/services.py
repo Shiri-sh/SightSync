4c0804ee2d8cf959d5ade4a2331cac01d470e337
shiri shahor <shirishahor@gmail.com>
2026-01-17 21:17:05 +0200

    change range

--- server/services.py
+++ server/services.py
@@ -5,9 +5,9 @@ import requests
 from transformers import CLIPProcessor, CLIPModel
 
 def get_status_from_score(score):
-    if score > 0.8:
+    if score > 0.3:
         return "high match"
-    elif score > 0.5:
+    elif score > 0.2:
         return "potential match"
     else:
         return "no match"
