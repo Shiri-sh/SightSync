c68619f36a21f3aaec4fa2c1137c5a8bd89978d6
shiri shahor <shirishahor@gmail.com>
2026-01-18 19:14:01 +0200

    front

--- client/main.js
+++ client/main.js
@@ -1,9 +1,10 @@
 const form = document.getElementById("verify-form");
 const imageInput = document.getElementById("imageInput");
 const textInput = document.getElementById("textInput");
-const result = document.getElementById("result");
+//const result = document.getElementById("result");
 const preview = document.getElementById('imagePreview');
-
+const clipScoreDiv = document.getElementById('clip_score');
+const blipTextDiv = document.getElementById('blip_text');
 let filename;
 async function uploadImage() {
   if (!imageInput.files || imageInput.files.length === 0) return;
@@ -43,6 +44,21 @@ async function clipVerifyScore(filename, text) {
         console.log("Error getting clip score:", error.message);
     }
 }
+async function blipImgAnalyze(filename){
+  try {
+    const blipResponse = await fetch("http://localhost:8000/blip/blip_img_anlz", {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ image_name: filename }),
+    });
+    const data = await blipResponse.json();
+
+    console.log("Received data:", data);
+    return data;
+  } catch (error) {
+    console.log("Error getting BLIP analysis:", error.message);
+  }
+}
 imageInput.addEventListener('change', function(e) {
       const input = e.target;
       const file = input?.files?.[0];
@@ -61,19 +77,31 @@ imageInput.addEventListener('change', function(e) {
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-
+  blipTextDiv.textContent=""
+  clipScoreDiv.textContent=""
   //await uploadImage();
   filename="DSC_6831-258.jpg"
   const text = textInput.value;
 
   if (filename && text) {
-    const data = await clipVerifyScore(filename, text);
-    if (data.status!=="error"){ 
-      result.textContent = data.status;
+    clipScoreDiv.textContent="Loading..."
+
+    const clip_data = await clipVerifyScore(filename, text);
+    if (clip_data.status==="ok"){ 
+      clipScoreDiv.textContent = clip_data.analyze_score;
     }
     else{
-      result.textContent = data.message;
+      clipScoreDiv.textContent = clip_data.message;
     }
+    blipTextDiv.textContent="Loading..."
+    const blip_data = await blipImgAnalyze(filename, text);
+    if (blip_data.status==="ok"){
+      blipTextDiv.textContent = blip_data.text_blip;
+    }
+    else{
+      blipTextDiv.textContent = blip_data.message;
+    }
+
   }
   else{
      alert("Please provide both an image and text.");
