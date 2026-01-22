ad75eba12a8f32e2210d18f1b2eafdfb3e7f09fe
shiri shahor <shirishahor@gmail.com>
2026-01-22 21:32:28 +0200

    list_images

--- server/controllers/data.py
+++ server/controllers/data.py
@@ -13,6 +13,7 @@ async def upload_img(
         path = f"{DIR_PATH}/{image.filename}"
         with open(path, "wb") as f:
             f.write(await image.read())
+
         doc = {
             "filename": image.filename,
             "url": f"{image.filename}",
@@ -21,7 +22,7 @@ async def upload_img(
             "created_at": datetime.utcnow()
         }
 
-        await images.insert_one(doc)
+        images.insert_one(doc)
 
         return {"status": "ok", "filename": image.filename}
     
@@ -32,7 +33,7 @@ async def get_img(
 ):
     try:
         # path = f"{DIR_PATH}/{filename}"
-        image_doc = await images.find_one({"filename": filename})
+        image_doc =  images.find_one({"filename": filename})
         if image_doc:
             return {"status": "ok", "filename": filename, "url": image_doc["url"]}
         else:
@@ -42,7 +43,7 @@ async def get_img(
     
 async def list_images():
     try:
-        images_list = await images.find().to_list(100)
+        images_list = images.find()
         return {"status": "ok", "images": [{"url": img["url"], "filename": img["filename"]} for img in images_list]}
     except Exception as e:
         return {"status": "error", "message": str(e)}
