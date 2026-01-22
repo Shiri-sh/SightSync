7c9e78b01758a1f9cdf53cc5f21cee76b435a25c
shiri shahor <shirishahor@gmail.com>
2026-01-22 21:18:32 +0200

    get_img

--- server/controllers/data.py
+++ server/controllers/data.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from PIL import Image
 import os
+from datetime import datetime
+from mongoDB import images
+
 DIR_PATH = "images/"
 async def upload_img(
     image: UploadFile=File(...)
@@ -10,23 +13,36 @@ async def upload_img(
         path = f"{DIR_PATH}/{image.filename}"
         with open(path, "wb") as f:
             f.write(await image.read())
+        doc = {
+            "filename": image.filename,
+            "url": f"{image.filename}",
+            "embedding": None,
+            "captions": [],
+            "created_at": datetime.utcnow()
+        }
 
-            return {"status": "ok", "filename": image.filename}
+        await images.insert_one(doc)
+
+        return {"status": "ok", "filename": image.filename}
+    
     except Exception as e:
         return {"status": "error", "message": str(e)}
 async def get_img(
     filename: str = Form(...)
 ):
     try:
-        path = f"{DIR_PATH}/{filename}"
-        with open(path, "rb") as f:
-            return {"status": "ok", "filename": filename, "image": f.read()}
+        # path = f"{DIR_PATH}/{filename}"
+        image_doc = await images.find_one({"filename": filename})
+        if image_doc:
+            return {"status": "ok", "filename": filename, "url": image_doc["url"]}
+        else:
+            return {"status": "error", "message": "Image not found"}
     except Exception as e:
         return {"status": "error", "message": str(e)}
     
 async def list_images():
     try:
-        images = os.listdir(DIR_PATH)
-        return {"status": "ok", "images": [{"url": img, "filename": img} for img in images]}
+        images_list = await images.find().to_list(100)
+        return {"status": "ok", "images": [{"url": img["url"], "filename": img["filename"]} for img in images_list]}
     except Exception as e:
         return {"status": "error", "message": str(e)}
