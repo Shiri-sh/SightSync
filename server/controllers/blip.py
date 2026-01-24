fac8eefcf7f1d76862e50816f9b816dce67eed68
shiri shahor <shirishahor@gmail.com>
2026-01-24 20:42:45 +0200

    update caption of img

--- server/controllers/blip.py
+++ server/controllers/blip.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile, File, Form,Request
-
+from mongoDB import images
 from PIL import Image
-IMAGE_DIR = "images/"
+IMAGE_DIR = "images"
 
 async def blip_img_anlz(
     request: Request,
@@ -10,6 +10,14 @@ async def blip_img_anlz(
     try:
         img = Image.open(f"{IMAGE_DIR}/{data['image_name']}")
         text_blip=request.app.state.blip_img_analyzer.generate_caption(img)
+
+        # Append the new caption and keep only the most recent 3 captions
+        images.update_one(
+            {"filename": data['image_name']},
+            {"$push": {"captions": {"$each": [text_blip], "$slice": -3}}},
+            upsert=True
+        )
+
         return {
             "status": "ok",
             "filename": data['image_name'],
