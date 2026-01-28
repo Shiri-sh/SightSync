737ac732f1d4bb853bef43678d9bf23decc15eab
shiri shahor <shirishahor@gmail.com>
2026-01-28 21:14:55 +0200

    add seed and random

--- server/interface/BlipCaption.py
+++ server/interface/BlipCaption.py
@@ -1,33 +1,27 @@
 import requests
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
-
+import random 
+import torch
 class BlipCaption:
     def __init__(self):
         self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base", local_files_only=True)
         self.model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base", local_files_only=True)
 
-    def generate_caption(self, image: Image.Image, text: str = None) -> str:
+    def generate_caption(self, image: Image.Image, text: str = None, seed=None) -> str:
+        if seed is None:
+            seed = random.randint(0, 1_000_000)
+
+        torch.manual_seed(seed)
+
         inputs = self.processor(image, text, return_tensors="pt") if text else self.processor(image, return_tensors="pt")
-        out = self.model.generate(**inputs)
+        out = self.model.generate(
+            **inputs,
+            do_sample=True,
+            temperature=0.9,
+            top_p=0.95,
+        )
         generated_caption = self.processor.decode(out[0], skip_special_tokens=True)
         print("Generated caption:", generated_caption)
         return generated_caption
 
-# img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
-# raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
-
-# # conditional image captioning
-# text = "a photography of"
-# inputs = processor(raw_image, text, return_tensors="pt")
-
-# out = model.generate(**inputs)
-# print(processor.decode(out[0], skip_special_tokens=True))
-# # >>> a photography of a woman and her dog
-
-# # unconditional image captioning
-# inputs = processor(raw_image, return_tensors="pt")
-
-# out = model.generate(**inputs)
-# print(processor.decode(out[0], skip_special_tokens=True))
-# >>> a woman sitting on the beach with her dog
